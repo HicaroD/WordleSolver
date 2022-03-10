b7af6fc30a69cd2f33fb6086e1f472b828388319
HicaroD <hdanrlley1@gmail.com>
2022-03-09 21:00:27 -0300

    Adding `tokenizer` to analyze the word better

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,65 @@
 use std::fs;
+use std::io;
 
-#[allow(dead_code)]
-enum Status {
-    Correct(char),
-    Close(char),
-    NotFound(char),
+#[derive(Debug)]
+enum LetterStatus<'a> {
+    Correct(&'a str, usize),
+    Close(&'a str),
+    NotFound(&'a str),
 }
 
 fn get_word_list(file: &String) -> Vec<&str> {
-    let mut data = file.split('\n').collect();
+    let data = file.split('\n').collect();
     data
+}
+
+// TODO(Hícaro): Big and unclear, variable names aren't that good
+fn tokenize_word(word: &str) -> Vec<LetterStatus> {
+    let letters: Vec<&str> = word.split_whitespace().collect();
+
+    let pairs: Vec<(&str, &str)> = letters.into_iter().map(|pair| {
+        let current_pair: Vec<&str> = pair.split('-').collect();
+        (current_pair[0], current_pair[1])
+    }).collect();
+
+    let mut tokens: Vec<LetterStatus> = vec![];
+    for (i, pair) in pairs.into_iter().enumerate() {
+        let (letter, status) = pair; 
+        
+        match status {
+            "C" => tokens.push(LetterStatus::Correct(letter, i)),
+            "Cl" => tokens.push(LetterStatus::Close(letter)),
+            "Nf" => tokens.push(LetterStatus::NotFound(letter)),
+            token => panic!("Invalid token: {token}"), // TODO: Add better error handling
+        }
+    }
+
+    tokens
+}
+
+fn get_word() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Expected a string");
+    input
 }
 
 fn main() {
     let file = match fs::read_to_string("datasets/wordle_word_list.txt") {
         Ok(content) => content,
-        Err(err) => panic!("{}", err),
+        Err(err) => {
+            eprintln!("{err}");
+            std::process::exit(1);
+        }
     };
+    let wordle_words = get_word_list(&file);
 
-    let words = get_word_list(&file);
-    let filtered_words: Vec<&&str> = words.iter().filter(|word| word.contains('a') && word.contains('e')).collect();
-
-    for word in filtered_words.iter() {
-        println!("{}", word)
+    let mut i = 5;
+    while i != 0 {
+        println!("{}", get_word());
+        i-=1;
     }
-    println!("{}", filtered_words.len())
+
+    let tokens = tokenize_word(example);
 }
