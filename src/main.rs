b30cead6a725003351187679382110f9b29d7844
HicaroD <hdanrlley1@gmail.com>
2022-03-10 15:58:29 -0300

    Fixing bug when fetching all the matches
    I wasn't able to keep the words filtered from previous attempts because I wasn't changing the state of the original word list, after I started filtering the original list, I could see the list shrinking quickly and thus I was able to get wordle responses.

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ enum LetterStatus<'a> {
     NotFound(&'a str),
 }
 
-// TODO(Hícaro): Big and unclear, variable names aren't that good
-fn tokenize_word(word: &str) -> Vec<LetterStatus> {
+fn get_pairs(word: &str) -> Vec<(&str, &str)> {
     let letters: Vec<&str> = word.split_whitespace().collect();
 
     let pairs: Vec<(&str, &str)> = letters
@@ -20,12 +19,20 @@ fn tokenize_word(word: &str) -> Vec<LetterStatus> {
         })
         .collect();
 
+    pairs
+}
+
+// TODO(Hícaro): Big and unclear, variable names aren't that good
+fn tokenize_word(word: &str) -> Vec<LetterStatus> {
+    let pairs = get_pairs(word);
+
     let mut tokens: Vec<LetterStatus> = vec![];
-    for (i, pair) in pairs.into_iter().enumerate() {
+
+    for (position, pair) in pairs.into_iter().enumerate() {
         let (letter, status) = pair;
 
         match status {
-            "C" => tokens.push(LetterStatus::Correct(letter, i)),
+            "C" => tokens.push(LetterStatus::Correct(letter, position)),
             "Cl" => tokens.push(LetterStatus::Close(letter)),
             "Nf" => tokens.push(LetterStatus::NotFound(letter)),
             token => {
@@ -34,39 +41,34 @@ fn tokenize_word(word: &str) -> Vec<LetterStatus> {
             }
         }
     }
-
     tokens
 }
 
-// TODO(Hícaro): Eliminate code repetition
-fn get_matches<'a>(tokens: &'a Vec<LetterStatus>, wordle_words: &'a Vec<&str>) -> Vec<&'a &'a str> {
-    let mut filtered_wordle_words: Vec<&&str> = vec![];
+fn filter_words_with_matches<'a>(tokens: &'a Vec<LetterStatus>, wordle_words: &'a mut Vec<&str>) {
+    let mut matches: Vec<&str> = wordle_words.clone();
 
     for token in tokens.into_iter() {
-        let mut matches: Vec<&&str> = match token {
-            LetterStatus::Correct(letter, position) => wordle_words
-                .iter()
+        matches = match token {
+            LetterStatus::Correct(letter, position) => matches
+                .into_iter()
                 .filter(|word| {
                     word.contains(letter)
                         && word.chars().nth(*position).unwrap() == letter.chars().nth(0).unwrap()
                 })
                 .collect(),
 
-            LetterStatus::Close(letter) => wordle_words
-                .iter()
+            LetterStatus::Close(letter) => matches
+                .into_iter()
                 .filter(|word| word.contains(letter))
                 .collect(),
 
-            LetterStatus::NotFound(letter) => wordle_words
-                .iter()
+            LetterStatus::NotFound(letter) => matches
+                .into_iter()
                 .filter(|word| !word.contains(letter))
                 .collect(),
         };
-
-        filtered_wordle_words.append(&mut matches)
     }
-
-    filtered_wordle_words
+    *wordle_words = matches;
 }
 
 fn get_word() -> String {
@@ -87,17 +89,18 @@ fn main() {
         }
     };
 
-    let wordle_words = wordle_words_file.split('\n').collect();
+    let mut wordle_words = wordle_words_file.split('\n').collect();
 
     let mut i = 5;
     while i != 0 {
         let word = get_word();
         let tokenized_word = tokenize_word(&word);
-        let filtered_words = get_matches(&tokenized_word, &wordle_words);
+        filter_words_with_matches(&tokenized_word, &mut wordle_words);
 
-        for word in filtered_words.into_iter() {
+        for word in wordle_words.iter() {
             println!("{word}")
         }
+
         i -= 1;
     }
 }
