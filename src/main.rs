692f40046ed9ad8c3e565e3b228ee968d221b3c9
HicaroD <hdanrlley1@gmail.com>
2022-03-09 22:02:10 -0300

    Adding function to filter list of Wordle words

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,34 @@ fn get_word() -> String {
     input
 }
 
+// TODO(Hícaro): Eliminate code repetition
+fn get_matches<'a>(tokens: &'a Vec<LetterStatus>, wordle_words: &'a Vec<&str>) -> Vec<&'a &'a str> {
+    let mut filtered_wordle_words: Vec<&&str> = vec![];
+
+    for token in tokens.into_iter() {
+        let mut matches: Vec<&&str> = match token {
+            LetterStatus::Correct(letter, position) => {
+                wordle_words.iter()
+                    .filter(|word| word.contains(letter) && 
+                                   word.chars().nth(*position).unwrap() == letter.chars().nth(0).unwrap())
+                    .collect()
+            },
+
+            LetterStatus::Close(letter) => {
+                wordle_words.iter().filter(|word| word.contains(letter)).collect()
+            }, 
+
+            LetterStatus::NotFound(letter) => {
+                wordle_words.iter().filter(|word| !word.contains(letter)).collect()
+            }, 
+        };
+
+        filtered_wordle_words.append(&mut matches)
+    }
+
+    filtered_wordle_words
+}
+
 fn main() {
     let file = match fs::read_to_string("datasets/wordle_word_list.txt") {
         Ok(content) => content,
@@ -56,12 +84,18 @@ fn main() {
             std::process::exit(1);
         }
     };
+
     let wordle_words = get_word_list(&file);
 
     let mut i = 5;
     while i != 0 {
         let word = get_word();
         let tokenized_word = tokenize_word(&word);
+        let filtered_words = get_matches(&tokenized_word, &wordle_words);
+        
+        for word in filtered_words.into_iter() {
+            println!("{word}")
+        }
         i-=1;
     }
 }
